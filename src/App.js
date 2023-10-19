ad196317db57c9b9300861db80034f75a39c0825
pramodh-douglas <134018126+pramodh-douglas@users.noreply.github.com>
2023-10-19 00:12:25 -0700

    Added footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
           <Route path="signup" element={<Register />} />
         </Route>
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+	return (
+		<footer class="bg-light text-center text-lg-start">
+			<div class="text-center p-3" style={{ backgroundColor: "#ff9b94" }}>
+				© 2020 Copyright:
+				<a class="text-dark" href="https://mdbootstrap.com/">
+					MDBootstrap.com
+				</a>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
