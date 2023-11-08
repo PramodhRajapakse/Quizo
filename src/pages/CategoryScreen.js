0455415f34ae0a1902139df9c7620e67d3914b45
pramodh-douglas <134018126+pramodh-douglas@users.noreply.github.com>
2023-11-08 08:38:50 -0800

    merge conflicts resolved

--- src/pages/CategoryScreen.js
+++ src/pages/CategoryScreen.js
@@ -7,40 +7,40 @@ import { useEffect, useState } from "react";
 import { MDBBtn } from "mdb-react-ui-kit";
 
 const CategoryScreen = () => {
-    const [categories, setCategories] = useState([]);
-    const [selectedCat, setSelectedCat] = useState(null);
-    let navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [selectedCat, setSelectedCat] = useState(null);
+  let navigate = useNavigate();
 
-    useEffect(() => {
-        setCategories(cats.trivia_categories);
-    }, []);
+  useEffect(() => {
+    setCategories(cats.trivia_categories);
+  }, []);
 
-    const chooseCategory = (val) => {
-        setSelectedCat(val)
-    }
+  const chooseCategory = (val) => {
+    setSelectedCat(val)
+  }
 
-    const handleContinue = () => {
-        navigate("/details", { state: { category: selectedCat } });
-    }
+  const handleContinue = () => {
+    navigate("/details", { state: { category: selectedCat } });
+  }
 
-return (
+  return (
     <div>
-        <div className="container-fluid text-center">
-            <p className="heading">Welcome to Quizo</p>
-            <p className="detailText">Select category to start quiz</p>
-        </div>
-        <div className="categoryContainer">
-            {categories.map((category) => {
-                return (
-                    <CategoryButton key={category.id} label={category.name} onClick={() => {chooseCategory(category)}}/>
-                )
-            })}
-        </div>
-        <div className="d-flex justify-content-center">
+      <div className="container-fluid text-center">
+        <p className="heading">Welcome to Quizo</p>
+        <p className="detailText">Select category to start quiz</p>
+      </div>
+      <div className="categoryContainer">
+        {categories.map((category) => {
+          return (
+            <CategoryButton key={category.id} label={category.name} onClick={() => { chooseCategory(category) }} />
+          )
+        })}
+      </div>
+      <div className="d-flex justify-content-center">
         <MDBBtn onClick={handleContinue} className="d-flex btn btn-dark">Continue</MDBBtn>
-        </div>
+      </div>
     </div>
-)
+  )
 }
 
 export default CategoryScreen;
