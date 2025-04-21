830421790b14366e9a7e052bbf65d548da9c310c
pramodh-douglas <134018126+pramodh-douglas@users.noreply.github.com>
2025-04-20 17:48:41 -0700

    fixed homepage url issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import CategoryScreen from './pages/CategoryScreen';
